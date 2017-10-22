da0fdfe2961d2b304d8e3ef10ec2d9771dfc8259
Leplan Ludwig <leplan.ludwig@gmail.com>
2017-10-22 15:23:37 +0200

    hotfix() : regex decimal prix produit
    - fix css

--- gestionProduit/gestionProduit.js
+++ gestionProduit/gestionProduit.js
@@ -1,4 +1,4 @@
-let vm = new Vue({
+new Vue({
         el: "#app",
         data() {
             return {
@@ -20,7 +20,6 @@ let vm = new Vue({
                 notificationError: "",
                 errorForm: false,
                 editModalTitle: false,
-                formSnackbar: false,
                 couleurToShow: [],
                 seeColor: false,
                 genreList: ["Homme", "Femme", "Fille", "Garçon"],
@@ -49,9 +48,9 @@ let vm = new Vue({
                     poid: "",
                     prix: "",
                     printfacepossible: true,
-                    isActive : false,
+                    isActive: false,
                     prixpromotion: "",
-                    tailles : []
+                    tailles: []
 
                 },
                 headers: [
@@ -60,7 +59,7 @@ let vm = new Vue({
                     {text: 'Visuel', value: 'visuel'},
                     {text: 'Poid', value: 'poid'},
                     {text: 'Prix (TTC)', value: 'prix'},
-                    {text : 'Prix promotion (TTC)', value : 'prixpromotion'},
+                    {text: 'Prix promotion (TTC)', value: 'prixpromotion'},
                     {text: "Action", value: ""}
                 ],
                 faceavant: "",
@@ -69,29 +68,34 @@ let vm = new Vue({
                 items: [],
                 currentCouleur: {},
                 couleursList: [],
-                tailles : ['XS', 'S', 'M', 'L', 'XL', 'XXL']
+                tailles: ['XS', 'S', 'M', 'L', 'XL', 'XXL']
 
 
             }
+        },
+        mounted() {
+            this.getAllCouleur();
+            this.getAllGamme();
+            this.getAllImage();
+            this.getAllProduit();
         },
         methods: {
             /**
              * Ajoute un produit en base de donnée.
              */
-            addProduit: function (event) {
+            addProduit(event) {
                 this.produit.visuel = [this.faceavant, this.facearriere, this.cote];
-                vm.checkForm();
-                if (vm.errorArray.length > 0) {
-                    vm.formSnackbar = true;
-                    return vm.notificationError = vm.errorArray.toString();
+                this.checkForm();
+                if (this.errorArray.length > 0) {
+                    return swal('', "Les champs suivants sont incorrects ou manquants : " + this.errorArray.toString(), 'error');
                 }
-                vm.dialog = false;
-                vm.saveModal = true;
-                return this.$http.post(vm.baseUrlProduit, vm.produit).then((produit) => {
-                    vm.notificationError = "";
-                    vm.produit.key = produit.body.name;
-                    this.$http.put(vm.baseUrlEditProduit + produit.body.name + '.json', vm.produit).then((resp) => {
-                        vm.produit = {
+                this.dialog = false;
+                this.saveModal = true;
+                return this.$http.post(this.baseUrlProduit, this.produit).then((produit) => {
+                    this.notificationError = "";
+                    this.produit.key = produit.body.name;
+                    this.$http.put(`${this.baseUrlEditProduit + produit.body.name}.json`, this.produit).then((resp) => {
+                        this.produit = {
                             nom: "",
                             gamme: "",
                             genre: "",
@@ -99,124 +103,136 @@ let vm = new Vue({
                             visuel: [],
                             poid: "",
                             prix: "",
-                            isActive : false
+                            isActive: false
                         }
-                            vm.imgList.forEach(function (img) {
-                                img.isActive = false;
-                            });
-                            vm.snackbar = true;
-                        vm.successtext = "Le produit à été enregistré en base avec succés";
-                        vm.getAllProduit();
-                        return vm.saveModal = false;
+                        this.imgList.forEach(img => {
+                            img.isActive = false;
+                        });
+                        this.snackbar = true;
+                        this.successtext = "Le produit à été enregistré en base avec succés";
+                        this.getAllProduit();
+                        return this.saveModal = false;
                     });
                 });
             },
             /**
              * Récupére l'ensemble des produits en base de donnée.
              */
-            getAllProduit: function (event) {
-                vm.items = [];
-                return this.$http.get(vm.baseUrlProduit).then((resp) => {
+            getAllProduit(event) {
+                this.items = [];
+                return this.$http.get(this.baseUrlProduit).then((resp) => {
                     Object.keys(resp.data).forEach((key) => {
-                        vm.items.push(resp.data[key]);
+                        this.items.push(resp.data[key]);
                     })
                 })
             },
             /**
              * Récupére l'ensemble des gammes en base de donnée.
              */
-            getAllGamme: function (event) {
-                return this.$http.get(vm.baseUrlGamme).then((gammes) => {
-                    Object.keys(gammes.data).forEach(function (key) {
-                        vm.gammeList.push(gammes.data[key].name);
+            getAllGamme(event) {
+                return this.$http.get(this.baseUrlGamme).then((gammes) => {
+                    Object.keys(gammes.data).forEach(key => {
+                        this.gammeList.push(gammes.data[key].name);
                     });
                 })
             },
             /**
              * Récupére l'ensemble des couleurs en base de donnée.
              */
-            getAllCouleur: function (event) {
-                return this.$http.get(vm.baseUrlCouleur).then((couleur) => {
-                    Object.keys(couleur.data).forEach(function (key) {
-                        vm.couleurList.push(couleur.data[key].value);
+            getAllCouleur(event) {
+                this.$http.get(this.baseUrlCouleur).then((couleur) => {
+                    Object.keys(couleur.data).forEach(key => {
+                        this.couleurList.push(couleur.data[key].value);
                     });
                 })
             },
             /**
              * Check de l'état du formulaire avant l'envoie d'un nouveau produit
              */
-            checkForm: function () {
-                vm.errorArray = [];
-                if (vm.produit.gamme === "") {
-                    vm.errorArray.push("Gamme");
+            checkForm() {
+                this.errorArray = [];
+                if (this.produit.gamme === "") {
+                    this.errorArray.push("Gamme");
                 }
-                if (vm.produit.genre === "") {
-                    vm.errorArray.push("Genre")
+                if (this.produit.genre === "") {
+                    this.errorArray.push("Genre")
                 }
-                if (vm.produit.prix === "") {
-                    vm.errorArray.push("Prix");
+                if (this.produit.prix === "") {
+                    this.errorArray.push("Prix");
                 }
-                if (vm.produit.poid === "") {
-                    vm.errorArray.push("Poid");
+                if (this.produit.poid === "") {
+                    this.errorArray.push("Poid");
                 }
-                if (vm.produit.couleur.length === 0) {
-                    vm.errorArray.push("Couleur");
+                if (this.produit.couleur.length === 0) {
+                    this.errorArray.push("Couleur");
                 }
-                if (vm.produit.visuel.length === 0) {
-                    vm.errorArray.push("Visuel");
+                if (this.faceavant === "") {
+                    this.errorArray.push("Face avant");
                 }
-                if (vm.produit.visuel.length > 3) {
-                    vm.errorArray.push("3 visuels maximum par produit (coté, face avant, face arrière");
+                if (this.facearriere === "") {
+                    this.errorArray.push("Face arrière");
                 }
+                if (this.produit.cote === "") {
+                    this.errorArray.push("Côté");
+                }
+                console.log('jepasse')
+                this.checkDecimalPrice()
+            },
 
+            checkDecimalPrice() {
+                "use strict";
+                let priceArray = [this.produit.prix, this.produit.prixpromotion];
+                console.log(priceArray);
+                if (priceArray[0].indexOf(".")==-1 || priceArray[1].indexOf(".")==-1) {
+                    this.errorArray.push('Les prix doivent êtres indiqués en decimal');
+                }
             },
 
             /**
              * Permet de récuperer l'ensemble des images en base de donnée.
              */
-            getAllImage: function (event) {
-                return this.$http.get(vm.baseUrlImgList).then((img) => {
+            getAllImage(event) {
+                return this.$http.get(this.baseUrlImgList).then((img) => {
                     Object.keys(img.body).forEach((key) => {
-                        vm.imgList.push({base64: img.data[key].base64});
+                        this.imgList.push({base64: img.data[key].base64});
                     });
                 })
             },
             /**
              * Supprime le produit cliqué par l'utilisateur.
              */
-            deleteProduit: function ($event, currentProduit) {
+            deleteProduit($event, currentProduit) {
                 let produitToDelete = currentProduit;
-                return this.$http.delete(vm.baseUrlEditProduit + produitToDelete.key + ".json").then((resp) => {
-                    vm.deletesnackbar = true;
-                    return vm.getAllProduit();
+                return this.$http.delete(`${this.baseUrlEditProduit + produitToDelete.key}.json`).then((resp) => {
+                    this.deletesnackbar = true;
+                    return this.getAllProduit();
                 });
             },
             /**
              * Edit du produit selectionné par l'utilisateur.
              */
-            editProduit: function ($event) {
-                vm.checkForm();
-                if (vm.errorArray.length > 0) {
-                    vm.formSnackbar = true;
-                    return vm.notificationError = vm.errorArray.toString();
+            editProduit($event) {
+                this.checkForm();
+                if (this.errorArray.length > 0) {
+                    return swal('', "Les champs suivants sont incorrects ou manquants :" + this.errorArray.toString(), 'error');
                 }
-                vm.dialog = false;
-                vm.saveModal = true;
-                return this.$http.put(vm.baseUrlEditProduit + vm.produit.key + ".json", vm.produit).then((resp) => {
-                    vm.editsnackbar = true;
-                    vm.getAllProduit();
-                    return vm.saveModal = false;
+                this.dialog = false;
+                this.saveModal = true;
+                return this.$http.put(`${this.baseUrlEditProduit + this.produit.key}.json`, this.produit).then((resp) => {
+                    this.editsnackbar = true;
+                    this.getAllProduit();
+                    return this.saveModal = false;
                 });
             },
             /**
              * Récupere le produit que l'utilisateur selectionne.
              */
-            getCurrentProduit: function ($event, currentProduit) {
-                vm.editModalTitle = true;
-                vm.createModalTitle = false;
+            getCurrentProduit($event, currentProduit) {
+                this.editModalTitle = true;
+                this.createModalTitle = false;
                 $event.stopPropagation();
-                vm.dialog = true;
-                vm.produit = currentProduit;
+                this.dialog = true;
+                this.produit = currentProduit;
                 this.faceavant = this.produit.visuel[0];
                 this.facearriere = this.produit.visuel[1];
                 this.cote = this.produit.visuel[2];
@@ -225,34 +241,34 @@ let vm = new Vue({
             /**
              * Ajoute un visuel dans le tableau du produit
              */
-            addVisuelToProduit: function ($event, currentImage) {
-                vm.produit.visuel.push(currentImage);
+            addVisuelToProduit($event, currentImage) {
+                this.produit.visuel.push(currentImage);
                 return currentImage.isActive = true;
             },
             /**
              * Vide le tableau visuel de produit
              */
-            clearProductVisual: function ($event) {
-                vm.produit.visuel = [];
-                vm.imgList.forEach(function (img) {
+            clearProductVisual($event) {
+                this.produit.visuel = [];
+                this.imgList.forEach(img => {
                     img.isActive = false;
                 });
             },
             /**
              * Affiche la liste des visuels du produit selectionné par l'utilisateur
              */
-            showCurrentVisualOfproduct: function ($event, currentProduit) {
-                vm.visualToShow = currentProduit.item.visuel;
+            showCurrentVisualOfproduct($event, currentProduit) {
+                this.visualToShow = currentProduit.item.visuel;
             },
 
             /**
              * Ferme la modal et annule toute les actions relatives à celle ci
              */
             cancelAction() {
-                vm.dialog = false;
-                vm.editModalTitle = false;
-                vm.createModalTitle = true;
-                vm.produit = {
+                this.dialog = false;
+                this.editModalTitle = false;
+                this.createModalTitle = true;
+                this.produit = {
                     nom: "",
                     gamme: "",
                     genre: "",
@@ -261,41 +277,25 @@ let vm = new Vue({
                     poid: "",
                     prix: "",
                     printfacepossible: true,
-                    tailles : []
+                    tailles: []
                 };
                 this.faceavant = "";
                 this.facearriere = "";
                 this.cote = "";
-                vm.imgList.forEach(function (img) {
+                this.imgList.forEach(img => {
                     img.isActive = false;
                 })
             },
             /**
              * Affiche la liste des couleurs du produit selectionné par l'utilisateur
              */
-            showCurrentColorOfProduct: function ($event, currentProduit) {
-                vm.couleurToShow = [];
+            showCurrentColorOfProduct($event, currentProduit) {
+                this.couleurToShow = [];
                 currentProduit.item.couleur.forEach((couleur) => {
-                    vm.couleurToShow.push({"colorName": couleur});
+                    this.couleurToShow.push({"colorName": couleur});
                 });
-            },
-            /**
-             * Export de la base de donnée client
-             */
-            exportClientDatabase: function ($event) {
-                return this.$http.get(vm.baseUrlClient).then((clientFile) => {
-                    let link = document.createElement("a");
-                    link.download = "base de donnée client.txt";
-                    let data = "text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(clientFile.data));
-                    link.href = "data:" + data;
-                    link.click();
-                })
             }
         }
     }
 );
 
-vm.getAllCouleur();
-vm.getAllGamme();
-vm.getAllImage();
-vm.getAllProduit();
